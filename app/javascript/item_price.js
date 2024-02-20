0f57ad731d52f8f5c3fde56f530380365e2e1329
Anzu-mi <cherry_kitori@yahoo.co.jp>
2024-02-20 17:59:30 +0900

    本番環境＿修正2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,16 @@
 // 金額を入力した数値をpriceInputという変数に格納する
-window.addEventListener('turbo:load', () => {
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-const inputValue = priceInput.value;
-const addTaxDom = document.getElementById("add-tax-price")
-addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-const addProfitDom = document.getElementById("profit")
-addProfitDom.innerHTML = Math.floor(priceInput.value - (priceInput.value * 0.1));
-})
-});
+function post (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price")
+  addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+  console.log("test")
+  const addProfitDom = document.getElementById("profit")
+  addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1));
+  })
+};
 
+window.addEventListener('turbo:load', post)
+window.addEventListener('turbo:render', post)
 
