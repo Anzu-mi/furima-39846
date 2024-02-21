3399691ae3768a83832dc492b7873a65e58fe72e
Anzu-mi <cherry_kitori@yahoo.co.jp>
2024-02-21 14:29:22 +0900

    挙動確認_修正2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,18 +1,18 @@
-const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey)
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+  const pay = () => {
+    const publicKey = gon.public_key;
+    const payjp = Payjp(publicKey);
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-  
-  const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form');
+    form.addEventListener("submit", (e) => {
+      payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
         const token = response.id;
@@ -24,9 +24,10 @@ const pay = () => {
       expiryElement.clear();
       cvcElement.clear();
       document.getElementById("charge-form").submit();
+     });
+      e.preventDefault();
     });
-    e.preventDefault();
-  });
-};
-
+  };
+  
 window.addEventListener("turbo:load", pay);
+window.addEventListener('turbo:render', pay);
